20534d199fac8d013418b0e58523929d074a0fe1
Valentijn Stienen <43643917+valentijnstienen@users.noreply.github.com>
2020-03-21 16:34:58 +0100

    Update main.py

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -235,4 +235,7 @@ def update(attr, old, new):
 
 select.on_change('value', update) 
 l = layout(row(column(select), tabs), sizing_mode='stretch_width')
+
+layout = column(p,widgetbox(slider), widgetbox(button))
+curdoc().add_root(layout)
 #curdoc().add_root(l)
