70ba396ed7a7cf55a98df440a03302756d9ab058
Valentijn Stienen <43643917+valentijnstienen@users.noreply.github.com>
2020-03-21 14:54:36 +0100

    Update main.py

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -2,7 +2,10 @@ import os
 import pandas as pd
 import numpy as np
 import pathlib
+import requests
 
+from PIL import Image
+from io import BytesIO
 from bokeh.io import curdoc
 from bokeh.plotting import figure
 from bokeh.layouts import layout, row, column
@@ -14,7 +17,26 @@ from bokeh.models import DatetimeTickFormatter
 from bokeh.models.tools import HoverTool
 from bokeh.models.widgets import Select
 
-import pokefunctions
+#%%
+def generation_palette():
+    return {1:'#ACD36C', 2:'#DCD677', 3:'#9CD7C8', 4:'#B7A3C3', 5:'#9FCADF', 6:'#DD608C',  7:'#E89483'}
+
+def read_votes(path_data_file):  
+    # Read data.
+    df_votes = pd.read_excel(path_data_file, sheet_name='Form Responses 1')
+    # Rename columns.
+    df_votes.rename(columns={'Timestamp':'timestamp', 'What is your favourite Pokémon?':'vote'}, inplace=True)
+    # Remove any potential NaN.
+    df_votes.dropna(inplace=True)
+    return df_votes
+
+def process_pokemon_votes(df_votes, pokemon_name):
+    df_votes_pokemon = df_votes.query('vote=="' + pokemon_name + '"')
+    df_votes_pokemon = df_votes_pokemon.groupby(pd.Grouper(key='timestamp', freq='1h')).count()
+    df_votes_pokemon['timestamp'] = df_votes_pokemon.index
+    df_votes_pokemon['timestamp_h'] = df_votes_pokemon[['timestamp']].timestamp.dt.strftime('%H:%M')
+    df_votes_pokemon.index = np.arange(0, len(df_votes_pokemon))
+    return df_votes_pokemon
 
 # Define paths.
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -30,9 +52,9 @@ df_ranked = pd.read_csv(PATH_DATA/'df_ranked.csv', index_col=0)
 df = df_ranked.sort_index()
 
 #
-df_votes = pokefunctions.read_votes(PATH_DATA/'responses.xlsx')
-df_votes_init = pokefunctions.process_pokemon_votes(df_votes, 'Bulbasaur')
-df_votes_max = pokefunctions.process_pokemon_votes(df_votes, 'Charizard')
+df_votes = read_votes(PATH_DATA/'responses.xlsx')
+df_votes_init = process_pokemon_votes(df_votes, 'Bulbasaur')
+df_votes_max = process_pokemon_votes(df_votes, 'Charizard')
 
 #%%
 # Define tools.
