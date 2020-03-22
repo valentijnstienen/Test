8034d9a48eff52f09118fcb2d33ed73911ae4d35
Valentijn Stienen <43643917+valentijnstienen@users.noreply.github.com>
2020-03-22 20:47:54 +0100

    Update main.py

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -46,7 +46,7 @@ for i in range(0,len(geoj)):
     test = shapely.geometry.mapping(geoj.loc[i,'geometry'])
     if i == 32:
         continue      
-    #test['coordinates'] = set_precision(test['coordinates'], 2)
+    test['coordinates'] = set_precision(test['coordinates'], 2)
     geoj.loc[i,'geometry'] = shapely.geometry.shape(test)
 
 
